8a4021666eb3a08e822e330f74facdbe5af1355a
Gildas Cherruel <gildas@breizh.org>
2014-02-17 23:27:41 +0900

    removed comments

--- lib/puppet/provider/package/chocolatey.rb
+++ lib/puppet/provider/package/chocolatey.rb
@@ -48,14 +48,6 @@ Puppet::Type.type(:package).provide(:chocolatey, :parent => Puppet::Provider::Pa
     chocolatey :update, package_name, *options
   end
 
-  # from puppet-dev mailing list
-  # Puppet will call the query method on the instance of the package
-  # provider resource when checking if the package is installed already or
-  # not.
-  # It's a determination for one specific package, the package modeled by
-  # the resource the method is called on.
-  # Query provides the information for the single package identified by @Resource[:name].
-
   def query
     Puppet.debug "Querying #{@resource[:name]}"
     self.class.instances.each do |provider_chocolatey|
